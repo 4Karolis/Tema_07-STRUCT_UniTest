45a877c2eea286b7dd2c1a56ad3485ab38607cad
4Karolis <4karolis.t@gmail.com>
2022-04-03 20:35:00 +0300

    addad

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tema_08___Struckts___UnitTests
+{
+    struct Person
+    {
+        public string Name;
+        public string LastName;
+        public int Age;
+
+        public Person(string name, string lastName, int age)
+        {
+            Name = name;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public void SayMyFullName()
+        {
+            Console.WriteLine($"My name is: {Name} and my last name is: {LastName}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 
-namespace Tema_07_STRUCT_UniTest
+namespace Tema_08___Struckts___UnitTests
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region STRUCTS teorija
+
+            // BE KONSTRUKTORIAUS
+            Person person;
+
+            person.Name = "Petras";
+            person.LastName = "Petrauskas";
+            person.Age = 28;
+            person.SayMyFullName();
+
+            Person person2;
+
+            person2.Name = "Jonas";
+            person2.LastName = "Jonauskas";
+            person2.Age = 30;
+            person2.SayMyFullName();
+
+            //SU KONSTRUKTORIUM
+            //add.missing
+            Person person3 = new Person("Petras_CTOR", "Petrauskas_CTOR", 84);
+            person3.SayMyFullName();
+
+            #endregion
+
+            #region UZDUOTYS
+            //BE konstruktoriaus:
+            Uzduotis1 uzduotis1;
+            uzduotis1.Trimestras1 = 8;
+            uzduotis1.Trimestras2 = 10;
+            uzduotis1.Trimestras3 = 6;
+            uzduotis1.Vidurkis = (Trimestras1 + Trimestras2 + Trimestras3) / 3) ;
+            uzduotis1.IsveskVidurki();
+            int vidurkis = MetinisVidurkis();
+
+            #endregion
+
+            #region UnitTest
+
+
+
+            #endregion
+
+        }
+        public static decimal CalculatorTax(decimal income)
+        {
+            decimal result = 0;
+            if (income <= 40000)
+            {
+                result = income * 0.05m;
+            }
+            else if (income > 40000 && income <= 100000)
+            {
+                result = income * 0.15m;
+            }
+            else
+            {
+                result = income * 0.25m;
+            }
+            return result;
         }
     }
 }
