e940b2b942c0787f63623e7cf78abe1898160f5b
4Karolis <4karolis.t@gmail.com>
2022-04-03 20:21:09 +0300

    created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tema_07_STRUCT_UniTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
